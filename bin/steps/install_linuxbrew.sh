cb9390c7ae4487edd52a6d0865d79056005ed479
dmorse <dmorse@factset.com>
2017-04-03 23:27:04 +0100

    rationalizing buildpack

--- bin/steps/install_linuxbrew.sh
+++ bin/steps/install_linuxbrew.sh
@@ -14,11 +14,21 @@ if [ ! -x "$(which brew)" ]; then
 
     echo -e 'y\n' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" |& indent |& brew_quiet
 
+    puts-step "Aggressively optimizing git repo"
+    git --git-repo="$BUILD_DIR/.linuxbrew/.git" gc --aggressive
+
     puts-step "Installing GCC"
     brew install gcc |& indent |& brew_quiet
+
 else
+
     puts-step "Linuxbrew already installed"
     do-debug "which brew = $(which brew)"
+
+    puts-step "Auto-optimizing git repo"
+    git --git-repo="$BUILD_DIR/.linuxbrew/.git" gc --auto
+
     puts-step "Updating Linuxbrew"
     brew update |& indent |& brew_quiet
+
 fi
