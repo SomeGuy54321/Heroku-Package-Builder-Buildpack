1668b2bbba87987bd3b2032e86a760f1bc0ac611
dmorse <dmorse@factset.com>
2017-04-03 04:51:55 +0100

    figuring out where stuff is

--- bin/steps/install_linuxbrew.sh
+++ bin/steps/install_linuxbrew.sh
@@ -7,16 +7,6 @@ DEBUG_HERE=1
 # HOMEBREW_REPOSITORY: /app/.linuxbrew
 # HOMEBREW_CELLAR: /app/.linuxbrew/Cellar
 
-# linuxbrew bases its installation on the HOME variable
-# so TEMPORARILY we set this to the BUILD_DIR
-OLD_HOME=$HOME
-export HOME=$TMP_APP_DIR
-OLD_PATH=$PATH
-export PATH="$HOME/.linuxbrew/bin:$PATH"
-do-debug "PATH before checking if we install linuxbrew: '$PATH'"
-
-show_files
-show_linuxbrew
 
 if [ ! -x "$(which brew)" ]; then
     puts-step "Installing Linuxbrew"
@@ -32,9 +22,3 @@ else
     puts-step "Updating Linuxbrew"
     brew update | indent #| brew_quiet
 fi
-
-# reset to default locations
-export HOME=$OLD_HOME
-do-debug "HOME reset to '$HOME'"
-export PATH=$OLD_PATH
-do-debug "PATH reset to '$PATH'"
