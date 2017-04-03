8ef31da7233edbea543a09cb16eba8e0b7106ba5
dmorse <dmorse@factset.com>
2017-04-04 00:44:54 +0100

    rationalizing buildpack

--- bin/steps/install_linuxbrew.sh
+++ bin/steps/install_linuxbrew.sh
@@ -1,34 +1,24 @@
 #!/usr/bin/env bash
 
-DEBUG_HERE=1
-
-# defaults:
-# HOMEBREW_PREFIX: /app/.linuxbrew
-# HOMEBREW_REPOSITORY: /app/.linuxbrew
-# HOMEBREW_CELLAR: /app/.linuxbrew/Cellar
-
 
 if [ ! -x "$(which brew)" ]; then
+
     puts-step "Installing Linuxbrew"
     do-debug "Building linuxbrew in $HOME"
-
     echo -e 'y\n' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" |& indent |& brew_quiet
 
-    puts-step "Aggressively optimizing git repo"
-    git --git-dir="$BUILD_DIR/.linuxbrew/.git" gc --aggressive
+    git_commit_check
 
     puts-step "Installing GCC"
     brew install gcc |& indent |& brew_quiet
 
+    puts-step "Installing lz4"
+    brew install lz4 |& indent |& brew_quiet
+
 else
 
     puts-step "Linuxbrew already installed"
-    do-debug "which brew = $(which brew)"
-
-    puts-step "Auto-optimizing git repo"
-    git --git-dir="$BUILD_DIR/.linuxbrew/.git" gc --auto
-
-    puts-step "Updating Linuxbrew"
-    brew update |& indent |& brew_quiet
+    do-debug "\$(which brew)=$(which brew)"
+    git_commit_check
 
 fi
