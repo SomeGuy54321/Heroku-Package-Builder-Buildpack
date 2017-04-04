cad882789a333dfa2e0c577ff11ba3b21a3a57df
dmorse <dmorse@factset.com>
2017-04-04 06:15:35 +0100

    rationalizing buildpack

--- bin/steps/install_linuxbrew.sh
+++ bin/steps/install_linuxbrew.sh
@@ -7,7 +7,7 @@ if [ ! -x "$(which brew)" ]; then
     do-debug "Building linuxbrew in $HOME"
     echo -e 'y\n' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" |& indent |& brew_quiet
 
-    git_commit_check
+    git_checks_linuxbrew "$BUILD_DIR/.linuxbrew/.git"
 
     puts-step "Installing GCC"
     brew install gcc |& indent |& brew_quiet
@@ -19,7 +19,7 @@ else
 
     puts-step "Linuxbrew already installed"
     do-debug "\$(which brew)=$(which brew)"
-    git_commit_check
+    git_checks_linuxbrew "$BUILD_DIR/.linuxbrew/.git"
 
 fi
 
