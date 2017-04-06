2f7ca1233b9fd580292b4fa8e3da4298c8cc4230
dmorse <dmorse@factset.com>
2017-04-06 01:09:02 +0100

    fix exporting configvars

--- bin/steps/install_packages.sh
+++ bin/steps/install_packages.sh
@@ -16,17 +16,20 @@ function main() {
     eval $(parse_yaml $BUILD_DIR/package-extras.yaml "PACKAGE_EXTRAS_")
     #do-debug "Parsed YAML variables:"
     #debug_heavy "PACKAGE_EXTRAS_"
+    local REMAINING_PACKAGES
+    local REMAINING_UPACKAGES
     for package in ${PACKAGE_EXTRAS_packages[@]}; do
 
         if [ $(time_remaining) -gt 0 ]; then
 
             # only one formula allowed
-            FORMULAS_VAR="PACKAGE_EXTRAS_formulas_${package}"
-            FORMULAS="${!FORMULAS_VAR}"
+            local FORMULAS_VAR="PACKAGE_EXTRAS_formulas_${package}"
+            local FORMULAS="${!FORMULAS_VAR}"
             [ ${#FORMULAS} -eq 0 ] && FORMULAS=$package || true
 
             # multiple options flags allowed so add [@]
-            OPTIONS_VAR="PACKAGE_EXTRAS_options_${package}[@]"
+            local OPTIONS_VAR="PACKAGE_EXTRAS_options_${package}[@]"
+            local OPTIONS
             for opt in ${!OPTIONS_VAR}; do
                 OPTIONS="$OPTIONS --${opt}"
             done
@@ -36,7 +39,7 @@ function main() {
             brew_do install $FORMULAS $OPTIONS
 
             # multiple scripts can run
-            CONFIG_VAR="PACKAGE_EXTRAS_config_${package}[@]"
+            local CONFIG_VAR="PACKAGE_EXTRAS_config_${package}[@]"
             for script in ${!CONFIG_VAR}; do
                 run_user_script "$BUILD_DIR/$script"
             done
@@ -54,15 +57,23 @@ function main() {
     # uninstall things
     for upackage in ${PACKAGE_EXTRAS_uninstall[@]}; do
         if [ $(time_remaining) -gt 0 ]; then
-            brew_do uninstall $upackage
+
+            # only one formula allowed
+            local UFORMULAS_VAR="PACKAGE_EXTRAS_formulas_${upackage}"
+            local UFORMULAS="${!UFORMULAS_VAR}"
+            [ ${#UFORMULAS} -eq 0 ] && UFORMULAS=$upackage || true
+
+            puts-step "Uninstalling $upackage"
+            brew_do uninstall $UFORMULAS
+
         else
-            REMAINING_UNINSTALLS="$REMAINING_UNINSTALLS\n- $upackage"
+            REMAINING_UPACKAGES="$REMAINING_UPACKAGES\n- $upackage"
         fi
     done
 
-    if [ ${#REMAINING_UNINSTALLS} -gt 0 ]; then
+    if [ ${#REMAINING_UPACKAGES} -gt 0 ]; then
         puts-warn "The following packages did not uninstall in time:"
-        echo -e $REMAINING_UNINSTALLS |& indent | indent
+        echo -e $REMAINING_UPACKAGES |& indent | indent
         puts-warn "Try building again or increasing your PACKAGE_BUILDER_MAX_BUILDTIME configvar."
     fi
 
